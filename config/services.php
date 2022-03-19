74ce1aa2154c9a00c1668a7d6846125c00acbf41
sohamiith <sm21mtech14004@iith.ac.in>
2022-03-19 20:26:21 +0530

    composer

--- config/services.php
+++ config/services.php
@@ -29,5 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    'google' => [
+        'client_id' => '712829365918-nbhi00j4b6qshhpav3eh4pgs3j2m5dsf.apps.googleusercontent.com',
+        'client_secret' => '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',
+        'redirect' => 'http://127.0.0.1:8000/google/callback',
+    ],
 ];
